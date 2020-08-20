25ab2f0bbd96c66b2c6c005c4102231d19b8951e
roberto emanuel <kelvah@gmail.com>
2020-08-20 17:39:24 +0200

    DAAS-POC Added json viewer for api test responses

--- packages/online-editor/src/editor/TestAndDeploy.tsx
+++ packages/online-editor/src/editor/TestAndDeploy.tsx
@@ -3,7 +3,11 @@ import * as React from "react";
 import SwaggerClient from "swagger-client";
 import { useEffect, useState } from "react";
 import {
-  FormGroup,
+  Button,
+  Flex,
+  FlexItem,
+  Grid,
+  GridItem,
   Page,
   PageSection,
   Title,
@@ -11,8 +15,10 @@ import {
   Select,
   SelectOption,
   SelectVariant,
-  SelectDirection
+  SelectDirection,
+  Spinner
 } from "@patternfly/react-core";
+import ReactJson from "react-json-view";
 import Form from "@rjsf/bootstrap-4";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -20,6 +26,11 @@ import "./TestAndDeploy.css";
 
 interface TestAndDeployProps {
   showPanel: boolean;
+}
+interface ModelDeploy {
+  deployed: boolean;
+  waiting: boolean;
+  time?: string;
 }
 
 const TestAndDeploy = (props: TestAndDeployProps) => {
@@ -29,7 +40,9 @@ const TestAndDeploy = (props: TestAndDeployProps) => {
   const [isEndpointSelectOpen, setIsEndpointSelectOpen] = useState(false);
   const [selectedEndpoint, setSelectedEndpoint] = useState<Schema>();
   const [requestPayload, setRequestPayload] = useState();
-  const [responsePayload, setResponsePayload] = useState<string>();
+  const [responsePayload, setResponsePayload] = useState<{}>();
+  const [testEnvironment, setTestEnvironment] = useState(1);
+  const [modelDeploy, setModelDeploy] = useState<ModelDeploy>({ deployed: false, waiting: false });
 
   useEffect(() => {
     new SwaggerClient("http://localhost:8080/openapi").then((client: { spec: { paths: any } }) => {
@@ -49,6 +62,15 @@ const TestAndDeploy = (props: TestAndDeployProps) => {
     });
   }, []);
 
+  const handleDeploy = () => {
+    setTestEnvironment(1);
+    setModelDeploy({ deployed: false, waiting: true });
+    setTimeout(() => {
+      const now = new Date().toLocaleTimeString();
+      setModelDeploy({ deployed: true, waiting: false, time: now });
+    }, 2500);
+  };
+
   const onEndpointSelectToggle = (openStatus: boolean) => {
     setIsEndpointSelectOpen(openStatus);
   };
@@ -61,7 +83,7 @@ const TestAndDeploy = (props: TestAndDeployProps) => {
     setRequestPayload(formData);
 
     if (selectedEndpoint) {
-      setResponsePayload("");
+      setResponsePayload({});
       fetch("http://localhost:8080" + selectedEndpoint.url, {
         headers: {
           Accept: "application/json, text/plain",
@@ -87,48 +109,118 @@ const TestAndDeploy = (props: TestAndDeployProps) => {
         <div className="cd-panel__content">
           <Page>
             <PageSection>
-              <Title headingLevel="h3" size={TitleSizes["2xl"]} id="test-endpoints">
-                Test Things
+              <Title headingLevel="h4" size={TitleSizes.xl} style={{ marginBottom: "var(--pf-global--spacer--sm)" }}>
+                Deploy
               </Title>
-            </PageSection>
-            <PageSection>
-              <FormGroup label="Endpoint Selection" fieldId="endpoint-selection">
-                <Select
-                  id="endpoint-selection"
-                  variant={SelectVariant.single}
-                  aria-label="Select Input"
-                  onToggle={onEndpointSelectToggle}
-                  onSelect={onEndpointSelect}
-                  selections={selectedEndpoint}
-                  isOpen={isEndpointSelectOpen}
-                  aria-labelledby={"test-endpoints"}
-                  direction={SelectDirection.down}
-                >
-                  {schemas &&
-                    schemas.map((schema, index) => (
-                      <SelectOption key={index} value={schema}>
-                        {schema.url.replace(/%20/g, " ")}
-                      </SelectOption>
-                    ))}
-                </Select>
-              </FormGroup>
-            </PageSection>
-            <PageSection>
-              {selectedEndpoint && (
-                <Form schema={selectedEndpoint.schema} onSubmit={handleForm} formData={requestPayload} />
-              )}
-            </PageSection>
-            <PageSection>
-              <div className="form-group">
-                <label htmlFor="request-response">Response:</label>
-                <textarea
-                  className="form-control"
-                  id="request-response"
-                  rows={22}
-                  value={JSON.stringify(responsePayload, null, 2)}
-                  readOnly={true}
-                />
+              <div>
+                <Flex>
+                  <FlexItem>
+                    <Button type="button" variant="primary" onClick={handleDeploy} isDisabled={modelDeploy.waiting}>
+                      Publish Model
+                    </Button>
+                  </FlexItem>
+                  <FlexItem>
+                    {modelDeploy.waiting && (
+                      <span>
+                        <Spinner size="md" style={{ marginRight: ".5em" }} /> <em>Deploying</em>
+                      </span>
+                    )}
+                    {modelDeploy.deployed && <em>Last published today at {modelDeploy.time}</em>}
+                  </FlexItem>
+                </Flex>
               </div>
+            </PageSection>
+            <PageSection>
+              <Title headingLevel="h4" size={TitleSizes.xl} style={{ marginBottom: "var(--pf-global--spacer--sm)" }}>
+                Test
+              </Title>
+              <Grid hasGutter={true}>
+                <GridItem span={6}>
+                  <div className="endpoint-selection">
+                    <label htmlFor="endpoint-selection">Endpoint Selection</label>
+                    <Select
+                      id="endpoint-selection"
+                      variant={SelectVariant.single}
+                      aria-label="Select Input"
+                      onToggle={onEndpointSelectToggle}
+                      onSelect={onEndpointSelect}
+                      selections={selectedEndpoint}
+                      isOpen={isEndpointSelectOpen}
+                      aria-labelledby={"test-endpoints"}
+                      direction={SelectDirection.down}
+                    >
+                      {schemas &&
+                        schemas.map((schema, index) => (
+                          <SelectOption key={index} value={schema}>
+                            {schema.url.replace(/%20/g, " ")}
+                          </SelectOption>
+                        ))}
+                    </Select>
+                  </div>
+                </GridItem>
+                <GridItem span={6}>
+                  <label>Select environment</label>
+                  <Flex>
+                    <FlexItem>
+                      <Button
+                        variant={testEnvironment === 1 ? "primary" : "control"}
+                        isActive={testEnvironment === 1}
+                        onClick={() => setTestEnvironment(1)}
+                      >
+                        Development
+                      </Button>
+                    </FlexItem>
+                    <FlexItem>
+                      <Button
+                        variant={
+                          testEnvironment === 2
+                            ? "primary"
+                            : !modelDeploy.deployed || modelDeploy.waiting
+                            ? "secondary"
+                            : "control"
+                        }
+                        isActive={testEnvironment === 2}
+                        onClick={() => setTestEnvironment(2)}
+                        isDisabled={!modelDeploy.deployed || modelDeploy.waiting}
+                        title={"Only available after successful deploy"}
+                      >
+                        Production
+                      </Button>
+                    </FlexItem>
+                  </Flex>
+                </GridItem>
+              </Grid>
+            </PageSection>
+            <PageSection>
+              <Grid hasGutter={true}>
+                <GridItem span={6}>
+                  {selectedEndpoint && (
+                    <Form
+                      schema={selectedEndpoint.schema}
+                      onSubmit={handleForm}
+                      formData={requestPayload}
+                      className="dynamic-form"
+                    />
+                  )}
+                </GridItem>
+                <GridItem span={6}>
+                  <div className="endpoint-response">
+                    <label>Response</label>
+                    <div className="response-viewer">
+                      {responsePayload && (
+                        <ReactJson
+                          src={responsePayload}
+                          displayDataTypes={false}
+                          displayObjectSize={false}
+                          enableClipboard={false}
+                          name={false}
+                          theme="shapeshifter:inverted"
+                        />
+                      )}
+                    </div>
+                  </div>
+                </GridItem>
+              </Grid>
             </PageSection>
           </Page>
         </div>
